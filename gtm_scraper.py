9c22042f5949774379b6cff8be7a3a3a81443a1c
Leti <leti.garcia@barrelny.com>
2020-11-10 17:49:01 -0500

    starting to work on scraping tag column

--- gtm_scraper.py
+++ gtm_scraper.py
@@ -19,3 +19,13 @@ def site_login():
     driver.find_element_by_name('password').send_keys(password)
     driver.find_element_by_id('passwordNext').click()
     time.sleep(3)
+
+def get_tags():
+    for table in driver.find_elements_by_xpath('//*[contains(@id,"ID")]//tr'):
+        data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
+        print(data)
+    # time.sleep(3)
+
+# site_login()
+# get_tags()
+driver.quit()
