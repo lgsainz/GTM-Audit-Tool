a1c9ff1f98dc70588a5ea3c4937f33de8f66ffb8
Leti <leti.garcia@barrelny.com>
2020-11-11 15:54:13 -0500

    added functions to get account and tag page

--- gtm_scraper.py
+++ gtm_scraper.py
@@ -8,24 +8,29 @@ driver = webdriver.Chrome('/Users/Shared/chromedriver')
 username = ''
 password = ''
 url = ''
+account_to_audit = ''
+
 
 def site_login():
     driver.get(url)
+    driver.implicitly_wait(10) # this sets the default wait time for the rest of the WebDriver obect's life
+
     driver.find_element_by_id('identifierId').send_keys(username)
     driver.find_element_by_id('identifierNext').click()
-    time.sleep(1)
     driver.find_element_by_class_name('vxx8jf').click()
-    time.sleep(1)
     driver.find_element_by_name('password').send_keys(password)
     driver.find_element_by_id('passwordNext').click()
-    time.sleep(3)
+
+def get_account():
+    # click into the account you want to audit
+    driver.find_element_by_link_text(account_to_audit).click()
 
 def get_tags():
-    for table in driver.find_elements_by_xpath('//*[contains(@id,"ID")]//tr'):
-        data = [item.text for item in table.find_elements_by_xpath(".//*[self::td or self::th]")]
-        print(data)
-    # time.sleep(3)
+    get_account()     
+    # click into Tags from side menu
+    driver.find_element_by_class_name('open-tag-list-button').click()
 
-# site_login()
-# get_tags()
-driver.quit()
+
+site_login()
+get_tags()
+# driver.quit()
