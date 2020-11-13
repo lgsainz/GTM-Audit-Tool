3f3c7f6b21a9f6b35b738f41ed22278b8c66238d
Leti <leti.garcia@barrelny.com>
2020-11-13 15:25:34 -0500

    fixed bug when writing to csv

--- gtm_scraper.py
+++ gtm_scraper.py
@@ -48,12 +48,11 @@ def get_tags():
 
             # print(category, action, label)
             driver.find_element_by_class_name('gtm-sheet-header__close').click() # click back to tags view
-
-        else:
-            continue  
         
         # write values for each column to csv file
         f1.write(tag_name.text + ',' + category + ',' + action + ',' + label + ',' + trigger.text + '\n')
+        time.sleep(0.5) # to prevent skipping tags
+        
     f1.close()
         
 # create csv files
